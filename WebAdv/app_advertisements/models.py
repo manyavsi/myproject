6d2be9efdb673316fb4f20927de7509b33741ae1
manyavsi <137795860+manyavsi@users.noreply.github.com>
2023-08-21 02:07:54 +1000

    Add files via upload

--- WebAdv/app_advertisements/models.py
+++ WebAdv/app_advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
@@ -7,6 +10,25 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='заголовок ёмаё')
+    def yellow_title(self):
+            return format_html('<span style="color: yellow; font-weight: bold;">{}</span>', self.title)
+
+
+    @admin.display(description='дата сотворения')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+    @admin.display(description='дата последнего изменения боже')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self) -> str:
         return f"Advertisements(id={self.id}, title={self.title}, price={self.price})" # type: ignore
